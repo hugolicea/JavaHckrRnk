1cd9cc56ffed6442a0bf7bc4719bdd3149ffa5a6
Hugo Licea <h.leon@elranchoinc.com>
2022-01-06 17:55:11 -0600

    HackerRanck Algorithms: Diagonal Diference in Java 8

---
+++ DiagonalDiference/src/solution/Result.java
@@ -0,0 +1,26 @@
+package solution;
+
+import java.util.List;
+
+public class Result {
+	/*
+	 * The function is expected to return an INTEGER. The function accepts
+	 * 2D_INTEGER_ARRAY arr as parameter.
+	 */
+
+	public static int diagonalDifference(List<List<Integer>> arr) {
+		int firstDiagonal = 0;
+		int secondDiagonal = 0;
+
+		for (int i = 0; i < arr.size(); i++) {
+			firstDiagonal += arr.get(i).get(i);
+		}
+		for (int i = 0; i < arr.size(); i++) {
+			secondDiagonal += arr.get(i).get(arr.size() - (i + 1));
+		}
+
+		return Math.abs(firstDiagonal - secondDiagonal);
+
+	}
+
+}
